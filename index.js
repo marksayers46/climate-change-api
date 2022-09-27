d0493ebdcfb81059e1f352631418876b35b69825
Mark Sayers <marksayers46@icloud.com>
2022-09-27 11:26:33 -0500

    setting up get requests using axios

--- index.js
+++ index.js
@@ -6,4 +6,30 @@ const PORT = 8000
 
 const app = express()
 
+const articles = []
+
+app.get('/', (req,res) => {
+    res.json('Welcome to my Climate Change API')
+})
+
+app.get('/news', (req,res) => {
+    axios.get('https://www.theguardian.com/environment/climate-crisis')
+        .then((response) => {
+            const html = response.data
+            //console.log(html)
+            const $ = cheerio.load(html)
+
+            $('a:contains("climate")', html).each(function () {
+                const title = $(this).text()
+                const url = $(this).attr()
+                articles.push({
+                    title,
+                    url
+                })
+            })
+            res.json(articles)
+        })
+})
+
 app.listen(PORT, () => console.log(`PORT is running on: ${PORT}`))
+
